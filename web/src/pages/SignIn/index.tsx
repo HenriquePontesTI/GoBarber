5034637cec2dcde6559b3335c61c8436a0346bf2
Henrique Marques Lopes de Pontes <henrique.pontes@radixeng.com.br>
2021-02-27 09:32:45 -0300

    componentização dos inputs

--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -13,11 +16,11 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça Seu logon</h1>
 
-        <input placeholder="E-mail" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-        <input type="password" placeholder="Senha" />
+        <Input name="senha" icon={FiLock} type="password" placeholder="Senha" />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
@@ -30,6 +33,6 @@ const SignIn: React.FC = () => (
 
     <Background />
   </Container>
-)
+);
 
 export default SignIn;
